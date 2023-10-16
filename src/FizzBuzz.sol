9f8536c45b010dc28e1c188c6de9caf9b9ab54d7
Git Comit <git.comit@gmail.com>
2023-10-16 12:35:53 -0400

    wip

--- src/FizzBuzz.sol
+++ src/FizzBuzz.sol
@@ -1,41 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import {LibString} from "@solady/src/utils/LibString.sol";
+
 contract FizzBuzz {
     function fizzBuzz(uint256 n) public pure returns (string memory) {
+        if (n % 3 == 0 && n % 5 == 0) {
+            return "fizzbuzz";
+        }
         if (n % 3 == 0) {
             return "Fizz";
         }
         if (n % 5 == 0) {
             return "Buzz";
         } else {
-            return uintToString(n);
+            return LibString.toString(n);
         }
-    }
-
-    function uintToString(uint256 v) internal pure returns (string memory) {
-        if (v == 0) return "0";
-
-        uint256 j = v;
-        uint256 length;
-
-        while (j != 0) {
-            length++;
-            j /= 10;
-        }
-
-        bytes memory bstr = new bytes(length);
-        int256 k = int256(length) - 1; // change k to int256 to allow negative values
-
-        while (v != 0) {
-            bstr[uint256(k)] = bytes1(uint8(48 + v % 10)); // cast k back to uint256 for indexing
-            if (k > 0) {
-                // check if k is greater than 0 before decrementing
-                k--;
-            }
-            v /= 10;
-        }
-
-        return string(bstr);
     }
 }
--- test/FizzBuzzTest.t.sol
+++ test/FizzBuzzTest.t.sol
@@ -13,13 +13,30 @@ contract FizzBuzzTest is DSTest {
 
     function test_returnsFizzWhenDicisibleByThree() public {
         assertEq(fizzBuzz.fizzBuzz(3), "Fizz");
+        assertEq(fizzBuzz.fizzBuzz(6), "Fizz");
+        assertEq(fizzBuzz.fizzBuzz(9), "Fizz");
+        assertEq(fizzBuzz.fizzBuzz(27), "Fizz");
     }
 
     function test_returnsBuzzWhenDicisibleByFive() public {
         assertEq(fizzBuzz.fizzBuzz(5), "Buzz");
+        assertEq(fizzBuzz.fizzBuzz(10), "Buzz");
+        assertEq(fizzBuzz.fizzBuzz(605), "Buzz");
+        assertEq(fizzBuzz.fizzBuzz(25), "Buzz");
+        assertEq(fizzBuzz.fizzBuzz(455), "Buzz");
     }
 
     function test_returnsNumberAsStringOtherwise() public {
         assertEq(fizzBuzz.fizzBuzz(1), "1");
+        assertEq(fizzBuzz.fizzBuzz(1001), "1001");
+        assertEq(fizzBuzz.fizzBuzz(34), "34");
+        assertEq(fizzBuzz.fizzBuzz(91), "91");
+    }
+
+    function test_returns_fizzbuzz_when_divisible_by_three_and_five() public {
+        assertEq(fizzBuzz.fizzBuzz(15), "fizzbuzz");
+        assertEq(fizzBuzz.fizzBuzz(705), "fizzbuzz");
+        assertEq(fizzBuzz.fizzBuzz(900), "fizzbuzz");
+        assertEq(fizzBuzz.fizzBuzz(660), "fizzbuzz");
     }
 }
