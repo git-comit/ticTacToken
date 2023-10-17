0a64141f644f9d02f5f7df7aa51274544397ac7b
git-comit <git.comit@gmail.com>
2023-10-17 18:10:20 -0400

    updated deploy script for token

--- script/DeployTicTacToken.s.sol
+++ script/DeployTicTacToken.s.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.20;
 
 import {Script} from "forge-std/Script.sol";
 import {TicTacToken} from "../src/TicTacToken.sol";
+import {Token} from "../src/Token.sol";
 
 /**
  * @title DeployWhaleConfig
@@ -10,10 +11,11 @@ import {TicTacToken} from "../src/TicTacToken.sol";
  */
 
 contract DeployTicTacToken is Script {
-// function run() external returns (TicTacToken) {
-//     vm.startBroadcast();
-//     TicTacToken ticTacToken = new TicTacToken(msg.sender);
-//     vm.stopBroadcast();
-//     return ticTacToken;
-// }
+    function run() external returns (Token, TicTacToken) {
+        vm.startBroadcast();
+        Token token = new Token();
+        TicTacToken ticTacToken = new TicTacToken(msg.sender, address(token));
+        vm.stopBroadcast();
+        return (token, ticTacToken);
+    }
 }
